b9ab129e1522e8c62dc6d64d483ade59e2e79d5e
H12 <contact@henrynelsonfirth.com>
2024-08-18 20:07:01 -0500

    Get catppuccin theme working w/ nvim-web-devicons

--- lua/custom/plugins/catppuccin.lua
+++ lua/custom/plugins/catppuccin.lua
@@ -7,4 +7,35 @@ return {
       vim.cmd.colorscheme 'catppuccin-macchiato'
     end,
   },
+  {
+    'dgox16/devicon-colorscheme.nvim',
+    dependencies = {
+      'nvim-tree/nvim-web-devicons',
+    },
+    config = function()
+      local macchiato = require('catppuccin.palettes').get_palette 'macchiato'
+
+      require('devicon-colorscheme').setup {
+        colors = {
+          blue = macchiato.blue,
+          cyan = macchiato.teal,
+          green = macchiato.green,
+          magenta = macchiato.pink,
+          orange = macchiato.maroon,
+          purple = macchiato.mauve,
+          red = macchiato.red,
+          white = macchiato.subtext0,
+          yellow = macchiato.yellow,
+          bright_blue = macchiato.blue,
+          bright_cyan = macchiato.teal,
+          bright_green = macchiato.green,
+          bright_magenta = macchiato.pink,
+          bright_orange = macchiato.maroon,
+          bright_purple = macchiato.mauve,
+          bright_red = macchiato.red,
+          bright_yellow = macchiato.yellow,
+        },
+      }
+    end,
+  },
 }
