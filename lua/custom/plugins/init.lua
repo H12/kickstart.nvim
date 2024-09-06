91db66c62a9402b655243a81bb4034084f63de08
H12 <contact@henrynelsonfirth.com>
2024-09-06 09:28:40 -0500

    Add wtf.nvim for diagnostic explainers

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,4 +8,29 @@ return {
     dependencies = { 'nvim-treesitter/nvim-treesitter' },
     opts = {}, -- your configuration
   },
+  {
+    'piersolenski/wtf.nvim',
+    dependencies = {
+      'MunifTanjim/nui.nvim',
+    },
+    opts = {},
+    keys = {
+      {
+        'gw',
+        mode = { 'n', 'x' },
+        function()
+          require('wtf').ai()
+        end,
+        desc = 'Debug diagnostic with AI',
+      },
+      {
+        mode = { 'n' },
+        'gW',
+        function()
+          require('wtf').search()
+        end,
+        desc = 'Search diagnostic with Google',
+      },
+    },
+  },
 }
